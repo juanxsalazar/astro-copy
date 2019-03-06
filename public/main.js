9d72119c12c24297430ccabb62e197fc057ee7c4
Juan <juanxsalazar21@gmail.com>
2019-03-06 15:16:13 -0500

    restarting project

--- public/main.js
+++ public/main.js
@@ -1,7 +1,4 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+// const url = 'https://sdg-astro-api.herokuapp.com/api/'
+
 
 document.addEventListener('DOMContentLoaded', main)
